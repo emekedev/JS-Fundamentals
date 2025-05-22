1124eb79be18af2f9a9e6bdfb5286b510e0fc103
Nwannadi Emeke <emekedev@gmail.com>
2025-05-23 00:51:56 +0100

    Add 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,4 +1,13 @@
-function myFunction(val1, val2, val3){
-    console.log(myFunction('Python', 'is', 'fun' ))
-    console.log(myFunction('HBTN'))
+// 1. Declare a function named `sum` that takes two parameters, `num1` and `num2`.
+// 2. Inside the function, return the sum of `num1` and `num2`.
+// 3. After the function declaration, call the function with two numbers as arguments and store the result in a variable.
+// 4. Use `console.log()` to print the result to the console.
+
+function myFunction(val1, val2){
+    return val1 + " " + 'is' + " " + val2
 }
+
+let result = myFunction("Python", "fun");
+let test = myFunction('HBTN')
+console.log(result);
+console.log(test);

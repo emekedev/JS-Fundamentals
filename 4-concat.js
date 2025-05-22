138f125ef3d213404a3f1d0d62d4c5b091deaf8b
Nwannadi Emeke <emekedev@gmail.com>
2025-05-23 00:07:18 +0100

    Add 4-concat.js

--- 4-concat.js
+++ 4-concat.js
@@ -1,8 +1,10 @@
 const test1 = 'Python';
 const test2 = 'fun';
 const test3 = 'HBTN';
+const test4 = undefined;
 
 
 const args = process.argv.slice(2);
 
 console.log(`${args[test1]} is ${args[test2]}`);
+console.log(`${args[test3]} is ${args[test4]}`);
